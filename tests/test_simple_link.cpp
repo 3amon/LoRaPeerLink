4f5514ffa1f6077d527ce1cf1b40f95905608913
3amon <no@no.no>
2025-06-21 12:09:51 -0700

    fixing test

--- tests/test_simple_link.cpp
+++ tests/test_simple_link.cpp
@@ -12,14 +12,11 @@ TEST_CASE("LoRaBasicLink sends and receives") {
 
     MockRadio::clearChannel();
     MockRadio mock;
-    LoRaBasicLink link(mock);
-
-    // Simulate sending
-    link.send(reinterpret_cast<const uint8_t*>("hello"), 5);
-
+    mock.begin();
+    mock.send(reinterpret_cast<const uint8_t*>("hello"), 5);
     // Now try receiving
     uint8_t buffer[10];
-    size_t len = link.receive(buffer, sizeof(buffer));
+    size_t len = mock.receive(buffer, sizeof(buffer));
     REQUIRE(len == 5);
 
     MockRadio::clearChannel();
