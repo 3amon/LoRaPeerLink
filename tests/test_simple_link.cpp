da3bfa7d3c7f00510b004e617957e5b3bb733939
3amon <no@no.no>
2025-06-21 12:11:34 -0700

    fix

--- tests/test_simple_link.cpp
+++ tests/test_simple_link.cpp
@@ -9,14 +9,14 @@ uint32_t get_time_ms() { return mock_time; }
 void sleep_ms(uint32_t ms) { mock_time += ms; }
 
 TEST_CASE("LoRaBasicLink sends and receives") {
-
+    int len;
     MockRadio::clearChannel();
     MockRadio mock;
     mock.begin();
     mock.send(reinterpret_cast<const uint8_t*>("hello"), 5);
     // Now try receiving
     uint8_t buffer[10];
-    size_t len = mock.receive(buffer, sizeof(buffer));
+    len = mock.receive(buffer, sizeof(buffer));
     REQUIRE(len == 5);
 
     MockRadio::clearChannel();
